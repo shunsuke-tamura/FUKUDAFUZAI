103d2e8d1d2953b81da3e84edc2a6adc0cb6e06f
chouette11 <keifukuda1115@gmail.com>
2023-09-19 15:20:41 +0900

    Fix path

--- lib/model/entity/device_info/device_info_entity.dart
+++ lib/model/entity/device_info/device_info_entity.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:wordwolf/model/document/acc/acc_document.dart';
-import 'package:wordwolf/model/document/gyr/gyr_document.dart';
+import 'package:fukuda_fuzai/model/document/acc/acc_document.dart';
+import 'package:fukuda_fuzai/model/document/gyr/gyr_document.dart';
 
 part 'device_info_entity.freezed.dart';
 part 'device_info_entity.g.dart';
--- lib/provider/domain_providers.dart
+++ lib/provider/domain_providers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:wordwolf/page/root/root_page.dart';
+import 'package:fukuda_fuzai/page/root/root_page.dart';
 
 /// ページ遷移のプロバイダ
 final routerProvider = Provider<GoRouter>(
