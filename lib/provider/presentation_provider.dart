e350b44f668649d641355c8ba319e8251446a932
chouette11 <keifukuda1115@gmail.com>
2023-09-19 21:50:33 +0900

    Add providers

--- lib/provider/presentation_provider.dart
+++ lib/provider/presentation_provider.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nameTextFieldController = Provider((_) => TextEditingController());
+
+final qrCodeDataProvider = StateProvider((ref) => '');
 
 final xRouteProvider = StateProvider<double>((ref) => 0);
 
