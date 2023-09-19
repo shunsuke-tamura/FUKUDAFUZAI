89a373dbb1172436991af5bca41230a1e3b6cdc9
chouette11 <keifukuda1115@gmail.com>
2023-09-19 21:50:57 +0900

    Apply title and qr page

--- lib/provider/domain_providers.dart
+++ lib/provider/domain_providers.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fukuda_fuzai/page/qr/qr_page.dart';
+import 'package:fukuda_fuzai/page/title.dart';
 import 'package:go_router/go_router.dart';
 import 'package:fukuda_fuzai/page/root/root_page.dart';
 
@@ -8,13 +10,16 @@ final routerProvider = Provider<GoRouter>(
     routes: [
       GoRoute(
         path: '/',
-        builder: (context, state) => const RootPage(),
+        builder: (context, state) => const TitlePage(),
         routes: [
-          // GoRoute(
-          //   path: 'night',
-          //   builder: (context, state) =>
-          //       NightPage(roomId: state.extra! as String),
-          // ),
+          GoRoute(
+            path: 'qr',
+            builder: (context, state) => const QrPage(),
+          ),
+          GoRoute(
+            path: 'root',
+            builder: (context, state) => const RootPage(),
+          ),
         ],
       ),
     ],
